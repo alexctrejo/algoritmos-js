d27e11df9402d09c13c43f70b579ac1a277b7e69
Alexander Contreras <al20760432@ite.edu.mx>
2023-09-27 19:29:27 -0700

    Create fs.js

---
+++ fs.js
@@ -0,0 +1,18 @@
+const fs = require('node:fs');
+
+const array1 = [];
+
+fs.readFile('data.txt', 'utf-8', (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        let lines = data.split('\n');
+        for (const line in lines) {
+            console.log(lines[line].split(','))
+        }
+    }
+})
+
+
+
+
