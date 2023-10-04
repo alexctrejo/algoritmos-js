0e47ba3c1137bfc76e1af2400329f5457a79018e
Alexander Contreras <al20760432@ite.edu.mx>
2023-10-04 00:02:43 -0700

    Update gato-node.js
    funcionalidad terminada, falta agregar detalles pero funciona bien y registra en el archivo txt los cambios

--- gato-node.js
+++ gato-node.js
@@ -1,12 +1,85 @@
+const readline = require("node:readline")
+const fs = require("node:fs");
 
-const fs = require('fs');
+const rl = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout,
+})
 
 const tablero = [   
     0, 0, 0,
     0, 0, 0,
     0, 0, 0,    
 ];
+const jugador1 = 'X';
+const jugador2 = 'O';
 let encontradoVacio = true;
+let turno = true;
+let movimientos = 0
+
+function verificarVictoria(tablero, jugador) {
+    const lineasGanadoras = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontales
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Verticales
+        [0, 4, 8], [2, 4, 6] // Diagonales
+    ];
+
+    for (const linea of lineasGanadoras) {
+        const [a, b, c] = linea;
+        if (tablero[a] === jugador && tablero[b] === jugador && tablero[c] === jugador) {
+            return true; // El jugador ha ganado
+        }
+    }
+
+    return false; // El jugador no ha ganado todavía
+}
+
+function actualizarTablero(pos) {
+    if (tablero[pos] === 0) {
+        tablero[pos] = turno ? jugador1 : jugador2;
+        movimientos++;
+        mostrarTablero(tablero);
+
+        if (verificarVictoria(tablero, turno ? jugador1 : jugador2)) {
+            const nuevaLinea = `¡Jugador ${turno ? '1 (X)' : '2 (O)'} ha ganado!`
+            console.log(nuevaLinea);
+            rl.close();
+            fs.appendFile('configuracion.txt', nuevaLinea, 'utf-8', (err) => {
+                if (err) {
+                    console.error(`Error al escribir en el archivo: ${err}`);
+                } else {
+                    console.log('!Actualizacion!');
+                }
+            });
+            return;
+        }
+
+        if (movimientos === 9) {
+            console.log('Nadie gano dice pakotots!');
+            rl.close();
+            return;
+        }
+
+        turno = !turno;
+
+        const movimiento = turno ? 'X' : 'O';
+        const posicionEnTablero = pos + 1; // Sumamos 1 para mostrar posiciones de 1 a 9 en lugar de 0 a 8
+        const nuevaLinea = `Movimiento ${movimiento} en posición ${posicionEnTablero}\n`;
+        fs.appendFile('configuracion.txt', nuevaLinea, 'utf-8', (err) => {
+            if (err) {
+                console.error(`Error al escribir en el archivo: ${err}`);
+            } else {
+                console.log('\n!Actualizacion!');
+            }
+        });
+
+        jugar();
+
+    } else {
+        console.log('Esa posición ya está ocupada. Inténtalo de nuevo.');
+        jugar();
+    }
+}
 
 function mostrarTablero(tablero) {
     console.log('Tablero Actual:');
@@ -25,47 +98,26 @@ function verificarTablero(tablero) {
         }
     }
     if (encontradoVacio){
-        mostrarTablero(tablero)
+        mostrarTablero(tablero);
     }
 }
 
-function comprobarMovimientos(){
-    fs.readFile('configuracion.txt', 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
+function jugar() {
+
+    verificarTablero(tablero)
+
+    rl.question(`Jugador ${turno ? '1 (X)' : '2 (O)'}, ¿en qué posición quieres jugar? (1-9): `, (pos) => {
+        // Convierte la entrada del usuario a un índice de matriz (resta 1)
+        const posicion = parseInt(pos) - 1;
+        
+        // Verifica si la entrada es válida (entre 1 y 9)
+        if (posicion >= 0 && posicion < 9) {
+            actualizarTablero(posicion);
+        } else {
+            console.log('Entrada no válida. Introduce un número entre 1 y 9.');
+            jugar();
         }
-        console.log('Configuración inicial');
-        console.log(data);
-        console.log('\n');
-
-        const lineas = data.split('\n');
-        let movimientosLinea = lineas.find(linea => linea.includes('movimientos ='));
-        let movimientoXLinea = lineas.find(linea => linea.includes('movimiento_X ='));
-        let movimientoOLinea = lineas.find(linea => linea.includes('movimiento_O ='));
-        let nuevoValor;
-
-        fs.writeFile('configuracion.txt', nuevoValor, 'utf-8', err => {
-            if (err) {
-                console.error(`Error al escribir en el archivo: ${err}\n`);
-            } else {
-                fs.readFile('archivo.txt', 'utf-8', (err, newData) => {
-                    if (err) {
-                        console.error(`Error al leer el archivo: ${err}\n`);
-                    } else {
-                        console.log(`Contenido actualizado del archivo:\n ${newData}`);
-                    }
-                });
-            }
-        });
     });
 }
 
-function jugar(){
-    
-    verificarTablero(tablero)
-    comprobarMovimientos()
-    
-}
-
-jugar()
+jugar();
