6a6847079362a262580e2f0ddf6b9eedb15ee52f
Alexander Contreras <alexander@Alexanders-MacBook-Air.local>
2023-10-09 00:49:44 -0700

    reduje la complejidad en la lectura del parser - filtra directamente y despues comprueba que no queden espacios vacios en el filtro y se eliminan

--- parser.mjs
+++ parser.mjs
@@ -5,26 +5,11 @@ readFile('sql.txt', 'utf-8', (err, data) => {
         console.log(err)
     }
     
-       
-    const keywords = /^(SELECT|FROM|WHERE|CREATE|PROCEDURE|BEGIN|END)$/i;
-    const operators = /^(=|,|'|;|\(|\)|@)$/;
-   
-    
     const tokens = data
-        .split(/(\bSELECT\b|\bFROM\b|\bWHERE\b|\bCREATE\b|\bPROCEDURE\b|\bBEGIN\b|\bEND\b|,|'|\(|\)|;|\s+)/)
+        .split(/(,|'|\(|\)|;|:|_|\s+|\.)/)
         .filter(token => token && !token.match(/^\s+$/));
-
     console.log(tokens);
        
-    tokens.map(token => {
-        if (token.match(keywords)) {
-            console.log(`Palabra reservada: ${token}`);
-        } else if (token.match(operators)) {
-            console.log(`Operador: ${token}`);
-        } else {
-            console.log(`Palabra: ${token}`);
-        }
-    });
 });
    
    
