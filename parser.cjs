8f32f05b99fd158f76e47a2fa7ec1db8f82c456b
Alexander Contreras <alexander@Alexanders-MacBook-Air.local>
2023-10-19 17:36:12 -0700

    se hizo la asignacion de tokens y se añadio el  archivo de referencia donde estan los tokens que le corresponden a cada palabra

--- parser.cjs
+++ parser.cjs
@@ -2,16 +2,14 @@ const fs = require('node:fs')
 
 const simbolos = ['.', ',', '-', ' ', ';', '*', '(', ')', ':', "", '', '\n', '\r']; 
 const palabras = [];
-const coincidencias = [];
+const sqlKeywords = [];
 let palabraActual = "";
 
 fs.readFile('sql.txt', 'utf-8', (err, data) => {
     if (err) console.log(err);
-
     for (let i = 0; i < data.length - 1; i++) {
         if (simbolos.includes(data[i])) {
             if (palabraActual.length > 0) {
-                //falta añadir restricciones y cuando separar u no hacerlo.
                 if ( data[i] == "." && data[i + 1].match(/[a-zA-Z]/) ) {
                     palabraActual += data[i];
                 } else {
@@ -26,18 +24,22 @@ fs.readFile('sql.txt', 'utf-8', (err, data) => {
             palabraActual += data[i];
         }
     }
+});
 
-    fs.readFile('sqlkeywords.txt', 'utf-8', (err, data) => {
+fs.readFile('sqlkeywords.txt', 'utf-8', (err, data) => {
+        /* console.log(palabras); */
         if (err) {
             console.log(err);
         }
+        const keywords = data.split('\n');
         for (let i = 0; i < palabras.length; i++) {
-            if (data.includes(palabras[i])) {
-                console.log(`I found your word: ${palabras[i]}`);
-            } 
+            for (let j = 0; j < keywords.length; j++) {
+                let [id,  , valor] = keywords[j].split(' ');
+                if (palabras[i] == valor) {
+                    console.log(id);
+                }
+            }
             
         }
-        
-    });
-
-});
+    }
+);
