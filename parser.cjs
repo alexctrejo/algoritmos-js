8a9998732737921f76f1f5d4518ea3774e2171f8
Alexander Contreras <alexander@Alexanders-MacBook-Air.local>
2023-11-01 14:56:16 -0700

    correccion parser

--- parser.cjs
+++ parser.cjs
@@ -1,21 +1,16 @@
 const fs = require('node:fs')
 
-const simbolos = ['.', ',', '-', ' ', ';', '*', '(', ')', ':', "", '', '\n', '\r']; 
+const simbolos = ['.', ',', '-', ' ', ';', '*', '(', ')', ':', "", '', '\n', '\r', "'", '"']; 
 const palabras = [];
-const sqlKeywords = [];
 let palabraActual = "";
 
 fs.readFile('sql.txt', 'utf-8', (err, data) => {
     if (err) console.log(err);
-    for (let i = 0; i < data.length - 1; i++) {
+
+    for (let i = 0; i < data.length; i++) {
         if (simbolos.includes(data[i])) {
             if (palabraActual.length > 0) {
-                if ( data[i] == "." && data[i + 1].match(/[a-zA-Z]/) ) {
-                    palabraActual += data[i];
-                } else {
-                    palabras.push(palabraActual);
-                }
-                palabras.push(data[i]);
+                palabras.push(palabraActual);
                 palabraActual = ""; 
             } else {
                 palabras.push(data[i]);
@@ -26,20 +21,28 @@ fs.readFile('sql.txt', 'utf-8', (err, data) => {
     }
 });
 
+//Asignación de tokens
 fs.readFile('sqlkeywords.txt', 'utf-8', (err, data) => {
-        /* console.log(palabras); */
+        console.log(palabras);
         if (err) {
             console.log(err);
         }
+        //separación de los keywords por salto de linea
         const keywords = data.split('\n');
+        //recorrer las palabras y comparar con los keywords
         for (let i = 0; i < palabras.length; i++) {
             for (let j = 0; j < keywords.length; j++) {
+                //asignar el id del keyword a la palabra
                 let [id,  , valor] = keywords[j].split(' ');
+                //comparar la palabra con el valor del keyword y imprimir el id
                 if (palabras[i] == valor) {
                     console.log(id);
                 }
+                if (data[i] == typeof(Number)) {
+                    palabraActual += data[i];
+                    console.log(palabraActual + " es un numero");
+                }
             }
-            
         }
     }
 );
