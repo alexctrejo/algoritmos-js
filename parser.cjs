a7c27c59486463d6f397a50641b7e576c07520e6
Alexander Contreras <alexander@Alexanders-MacBook-Air.local>
2023-11-21 01:46:35 -0800

    Ajustar la lógica según la regla sintáctica - añadi archivos de prueba para comparar tokens con reglas.

--- parser.cjs
+++ parser.cjs
@@ -1,48 +1,77 @@
-const fs = require('node:fs')
+const fs = require('fs');
+const { reglasSintacticas } = require('./reglasSintacticas');
 
-const simbolos = ['.', ',', '-', ' ', ';', '*', '(', ')', ':', "", '', '\n', '\r', "'", '"']; 
+const simbolos = ['.', ',', '-', ' ', ';', '*', '(', ')', ':', '', '', '\n', '\r', "'", '"'];
 const palabras = [];
+const tokens = [];
 let palabraActual = "";
 
-fs.readFile('sql.txt', 'utf-8', (err, data) => {
+fs.readFile('select.txt', 'utf-8', (err, data) => {
     if (err) console.log(err);
-
     for (let i = 0; i < data.length; i++) {
         if (simbolos.includes(data[i])) {
             if (palabraActual.length > 0) {
                 palabras.push(palabraActual);
-                palabraActual = ""; 
-            } else {
+                palabraActual = "";
+            }
+            if (data[i] !== ' ') {  // Evitar agregar espacios adicionales
                 palabras.push(data[i]);
             }
         } else {
             palabraActual += data[i];
         }
     }
+
+    // Si hay una palabra al final sin espacio, agrégala
+    if (palabraActual.length > 0) {
+        palabras.push(palabraActual);
+    }
 });
 
-//Asignación de tokens
+// Asignación de tokens
 fs.readFile('sqlkeywords.txt', 'utf-8', (err, data) => {
-        console.log(palabras);
-        if (err) {
-            console.log(err);
+    console.log(palabras);
+    if (err) {
+        console.log(err);
+    }
+    // Separación de los keywords por salto de línea
+    const keywords = data.split('\n');
+    // Recorrer las palabras y comparar con los keywords
+    for (let i = 0; i < palabras.length; i++) {
+        let esPalabraReservada = false;
+
+        // Verificar si es una palabra reservada
+        for (let j = 0; j < keywords.length; j++) {
+            let [id, , valor] = keywords[j].split(' ');
+            if (palabras[i] === valor) {
+                tokens.push(id);
+                esPalabraReservada = true;
+                break;
+            }
         }
-        //separación de los keywords por salto de linea
-        const keywords = data.split('\n');
-        //recorrer las palabras y comparar con los keywords
-        for (let i = 0; i < palabras.length; i++) {
-            for (let j = 0; j < keywords.length; j++) {
-                //asignar el id del keyword a la palabra
-                let [id,  , valor] = keywords[j].split(' ');
-                //comparar la palabra con el valor del keyword y imprimir el id
-                if (palabras[i] == valor) {
-                    console.log(id);
-                }
-                if (data[i] == typeof(Number)) {
-                    palabraActual += data[i];
-                    console.log(palabraActual + " es un numero");
-                }
+
+        // Si no es una palabra reservada, considerarla como una cadena o número no reconocido
+        if (!esPalabraReservada) {
+            if (!isNaN(parseFloat(palabras[i])) && isFinite(palabras[i])) {
+                // Es un número, agregar el token correspondiente
+                tokens.push('98'); // Puedes usar otro código, como '98', para representar números no reconocidos
+            } else {
+                // Es una cadena no reconocida
+                tokens.push('99'); // Puedes usar otro código, como '99', para representar cadenas no reconocidas
             }
         }
     }
-);
+
+    console.log(tokens);
+
+    // Ajustar la lógica según la regla sintáctica.
+    let regla = 0;
+    if (tokens[0] == 655) {
+        regla = reglasSintacticas.SELECT;
+    } else if (tokens[0] == 772){
+        regla = reglasSintacticas.UPDATE1;
+    }
+    for (let k = 0; k < regla.length; k++) {
+        console.log(`token ${regla[k]} VALIDO`);
+    }
+});
